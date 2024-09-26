f689ad7273da679c62734980bef8e92e1d92c0ff
Akki-luffy <akhilkompally27@gmail.com>
2024-09-26 11:03:53 +0530

    Backend Change

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ import express, { json } from 'express'
 import puppeteer from 'puppeteer';
 import cors from 'cors';
 const app = express();
-const port = 5000;
-app.use(cors())
+const port = process.env.PORT || 5000;
+app.use(cors({origin:'https://leet-codolio-frontend.vercel.app/'}))
+
 async function WebScarpe(username) {
-    const browser = await puppeteer.launch({ headless: false});
+    const browser = await puppeteer.launch({ headless: true});
     try {
         const page = await browser.newPage();
         page.setDefaultTimeout(15000);
